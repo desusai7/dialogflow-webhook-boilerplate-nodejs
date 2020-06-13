f341fe9aea99a4693be00cbf512b76e5a0bef68f
Desu Sai Venkat <saivenkatdesu@gmail.com>
2020-06-13 02:56:09 +0000

    added my own intent

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = dialogflow({debug:true});
 app.intent('Default Welcome Intent', (conv)=>{
     conv.ask('Hello world!!');
 });
+app.intent('get train number',(conv,{number})=>{
+var result = number;
+return conv.close('The status of train number'+result+'is amazing !');
+});
 
 const expressApp = express().use(bodyParser.json());
 expressApp.post('/webhook',app);
