c0601dd9added073ec0925cbb4eb5fed8e664672
Desu Sai Venkat <saivenkatdesu@gmail.com>
2020-06-13 19:06:20 +0000

    Testing with url

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.intent('Default Welcome Intent', (conv)=>{
     conv.ask('Hello world!!');
 });
 app.intent('get train number',(conv,{number})=>{
+let data = fetch("https://your-first-herokus-app.herokuapp.com/status/02723/yesterday")
+.then(response => response.json())
+.then(jsondata=> {
+  result = jsondata.data;
+  console.log(result);});
+  
 return conv.close('The status of train number'+number+'is amazing!');
 });
 
