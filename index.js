dcac9afcaa7d7662e3a0bd14cde160c82f23f488
Desu Sai Venkat <saivenkatdesu@gmail.com>
2020-06-13 18:25:20 +0000

    Returning from then

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const {dialogflow} = require('actions-on-google');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 const app = dialogflow({debug:true});
+const fetch = require('node-fetch');
 
 app.intent('Default Welcome Intent', (conv)=>{
     conv.ask('Hello world!!');
 });
 app.intent('get train number',(conv,{number})=>{
 var result = number;
-return conv.close('The status of train number'+result+'is amazing !');
+let data = fetch("https://your-first-herokus-app.herokuapp.com/status/02723/yesterday")
+.then(response => response.json())
+.then(jsondata=> {
+  result = jsondata.data;
+  console.log(result);
+  return conv.close('The status of train number'+number+'is'+ result);
+});
 });
 
 const expressApp = express().use(bodyParser.json());
