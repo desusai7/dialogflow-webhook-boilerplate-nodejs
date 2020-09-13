14ce87be50e2fc5f51bc8a72735cc49b5d94029f
Desu Sai Venkat <saivenkatdesu@gmail.com>
2020-09-13 08:03:03 +0000

    Async and await

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.intent('Default Welcome Intent', (conv)=>{
     conv.ask('Hello world!!');
 });
 
+app.intent('get train number',async(conv,{number})=>{
+    var result;
+const resultdata =await fetch(`https://your-first-herokus-app.herokuapp.com/status/{$number}/yesterday`);
+conv.close('The status of train number'+number+'is amazing!'+result)
+/*.then(response => response.json())
+.then(jsondata=> {
+  result = jsondata.data;
+  console.log(result);
+return result;})
+.then(result => conv.close('The status of train number'+number+'is amazing!'+result));
+return conv.close('The status of train number'+number+'is amazing!');*/
+
+});
+
 const expressApp = express().use(bodyParser.json());
 expressApp.post('/webhook',app);
 
